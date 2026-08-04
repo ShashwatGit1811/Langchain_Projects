e799dd1334fb0f9c5e6c4ab44394584d8958b140
ShashwatGit1811 <acc18112007@gmail.com>
2026-08-04 16:34:22 +0530

    Improve code structure and response handling
    Refactor code for better readability and organization.

--- langchain_groq.py
+++ langchain_groq.py
@@ -4,31 +4,28 @@ from langchain.chat_models import init_chat_model
 from langchain_groq import ChatGroq
 from langchain_core.messages import HumanMessage,SystemMessage
 
+# loading env files
 load_dotenv()
 
+# Model declaration
 MODEL  = "llama-3.1-8b-instant"
 
 while True:
     prompt=input("You : ")
-    
     # print(prompt)
-
-    if prompt in ["Bye", "Nikal", "Milte Hai"]:
-        break
 
     #Method 1 :
     # llm = init_chat_model(MODEL,model_provider="groq")
 
     #Method 2:
     llm=ChatGroq(model=MODEL)
-
+    
     # print(llm)
-    #for Recive and Store in    Responce From server Or Model
+    #Recieving and Storing the response from model
     response = llm.invoke(prompt)
 
-    #for Taking Perticualr Output 
-    # print("Groq : ")
+    # Printing Output    
     print(response)
-    # print("Groq : ",response.content)
-    # print(response.usage_metadata)
-
+    print("Groq : ",response.content)
+    print(response.usage_metadata)
+    
