2e6588e7f1373527e8711166e9ae0d810e7a2504
ShashwatGit1811 <acc18112007@gmail.com>
2026-08-04 17:31:15 +0530

    Refactor code for clarity and organization

--- lc4.py
+++ lc4.py
@@ -6,22 +6,25 @@ prompts(System,Ai ans,Human prompts)
 
 import os
 from dotenv import load_dotenv
-from langchain.chat_models import init_chat_model
 from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate,MessagesPlaceholder 
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.memory import ConversationBufferWindowMemory
 
+# loading env files
 load_dotenv()
 
+# model defination
 MODEL  = "llama-3.1-8b-instant"
 
+# system prompt defination for llm
 SYSTEM_PROMPT = """
 You are an expert IT professional.
 You know everyhing about IT field and you are master of it.
 You have to be helpful and give concise answers.
 """
 
+# defination of llm
 llm=ChatGroq(
     model= MODEL,
     temperature=0.3,
@@ -29,12 +32,11 @@ llm=ChatGroq(
 )
 
 
-
-# TASK 1: Create memory
+# Creating memory
 memory=ConversationBufferWindowMemory(k=10,return_messages=True)
 
 
-# TASK 2: Build prompt
+# Build prompt
 prompt=ChatPromptTemplate.from_messages([
     ("system",SYSTEM_PROMPT),
     MessagesPlaceholder(variable_name="history"),
@@ -44,10 +46,10 @@ prompt=ChatPromptTemplate.from_messages([
 parser = StrOutputParser()
 
 
-# TASK 3: Build chain
+# Build chain
 chain=prompt|llm|parser
 
-# TASKS 4 & 5: Chat loop
+# chat bot infinte loop
 print("Type exit to quit")
 
 while True:
@@ -62,5 +64,3 @@ while True:
     memory.save_context({"input":user_input},{"output":reply})
 
     print(f"Ai : {reply}")
-    
-
